4df9b57e74f63e0abb15ce13aac2601a36af9110
Anthony Ngene <ngeneanthony@gmail.com>
2016-11-24 07:44:39 +0100

    Richard photography website

---
+++ richard/js/all.js
@@ -0,0 +1,101 @@
+(function($){
+    "use strict"; // Start of use strict
+    
+    
+    /* ---------------------------------------------
+     Scripts initialization
+     --------------------------------------------- */
+    
+    $(window).load(function(){
+        
+        // Page loader
+        
+        $("body").imagesLoaded(function(){
+            $(".page-loader div").fadeOut();
+            $(".page-loader").delay(200).fadeOut("slow");
+        });
+        
+        
+        initWorkFilter();
+        
+    });
+    
+    $(document).ready(function(){
+
+        init_masonry();
+    });
+
+
+    
+    
+    
+    
+})(jQuery); // End of use strict
+
+
+
+
+/* ---------------------------------------------
+ Portfolio section
+ --------------------------------------------- */
+
+// Projects filtering
+var fselector = 0;
+var work_grid = $("#work-grid, #isotope");
+
+function initWorkFilter(){
+    (function($){
+     "use strict";
+     var isotope_mode;
+     if (work_grid.hasClass("masonry")){
+         isotope_mode = "masonry";
+     } else{
+         isotope_mode = "fitRows"
+     }
+        
+        
+    })(jQuery);
+}
+
+
+
+/* ---------------------------------------------
+ Masonry
+ --------------------------------------------- */
+
+function init_masonry(){
+    (function($){    
+    
+        $(".masonry").imagesLoaded(function(){
+            $(".masonry").masonry();
+        });
+        
+    })(jQuery);
+}
+
+
+
+
+
+
+$('.work-item').colorbox({rel:'work-item', width:"100%", height:"100%", inline:true});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        
