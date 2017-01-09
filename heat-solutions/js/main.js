dbe2b512bddad9e1775925da4a95acb425096bb8
Anthony Ngene <ngeneanthony@gmail.com>
2017-01-09 18:29:24 +0100

    added the heat-solutions website - carlos

---
+++ heat-solutions/js/main.js
@@ -0,0 +1,96 @@
+$(document).ready(function(){
+    
+    //Page Loader
+    $("body").imagesLoaded(function(){
+        $(".page-loader div").fadeOut();
+        $(".page-loader").delay(200).fadeOut("slow");
+
+    });
+    
+    
+    //nav scroll snippet
+    $(function() {
+        $('a[href*="#"]:not([href="#"])').click(function() {
+            if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+                var target = $(this.hash);
+                target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+                if (target.length) {
+                    $('html, body').animate({
+                        scrollTop: target.offset().top
+                    }, 1000);
+                    return false;
+                }
+            }
+        });
+    });
+
+    //AJAX MAILER
+    $(function() {
+        $("#send_mail").click(function() {
+        	if($('#name').val() == ""){
+                alert('Please enter your Name');
+                return;
+            }
+            if($('#email').val() == ""){
+                alert('Please enter your E-mail');
+                return;
+            }
+            if($('#phone').val() == ""){
+                alert('Please enter your Phone Number');
+                return;
+            }
+
+            if($("#vinyl").prop('checked') == true){
+                var vinyl = "true"
+            }else{var vinyl = "false"}
+            if($("#dtg").prop('checked') == true){
+                var dtg = "true"
+                }else{var dtg = "false"}
+            if($("#silk").prop('checked') == true){
+                var silk = "true"
+                }else{var silk = "false"}
+            if($("#embroid").prop('checked') == true){
+                var embroid = "true"
+                }else{var embroid = "false"}
+            
+            var data = {
+                name: $("#name").val(),
+                
+                vinyl: vinyl,
+                dtg: dtg,
+                silk: silk,
+                embroid: embroid,
+                email: $("#email").val(),
+                phone: $("#phone").val(),
+                message: $("#message").val(),
+                send_mail: $("#send_mail").val(),
+            }
+            $.ajax({
+                type: "POST",
+                url: "send_mail.php",
+                data: data,
+                success: function(data){
+                    $('#thank-you').modal();
+                }
+            });
+
+        });
+    });
+    
+    
+    //calculate mobile-minty height and pad nav
+    var mobileLogoH = $('.mobile-minty').height() + 15;
+    $('.navbar-toggle').css('margin-top', mobileLogoH + 'px');
+    $(window).scroll(function (event) {
+        var scroll = $(window).scrollTop();
+        if(scroll == 0){
+            $('.navbar-toggle').css('margin-top', mobileLogoH + 'px');
+        }else{
+            $('.navbar-toggle').css('margin-top', '30px');
+        }
+    });
+    
+   
+    
+    
+});
---
+++ heat-solutions/send_mail.php
@@ -0,0 +1,42 @@
+<?php
+if(isset($_POST['send_mail'])):
+// from the form
+$name = trim(strip_tags($_POST['name']));
+$email = trim(strip_tags($_POST['email']));
+$dtg = $_POST['dtg'];
+$silk = $_POST['silk'];
+$embroid = $_POST['embroid'];
+$vinyl = $_POST['vinyl'];
+
+$message = 'Name: ' . $name . '<br>';
+$message .= 'Email: ' . $email . '<br>';
+$message .= 'Phone: ' . $phone . '<br>';
+
+$services = "";
+$services .= ($dtg == "true") ? 'Bed Bugs, ': "";
+$services .= ($silk == "true") ? 'W.D.Os, ': "";
+$services .= ($embroid == "true") ? 'V.O.Cs, ': "";
+$services .= ($vinyl == "true") ? 'Other, ': "";
+if($services == ""){
+    $services = "none selected";
+}
+$message .= "Service(s): ". $services. "<br><br>";
+
+$message .= 'Message: ' . htmlentities($_POST['message']);
+
+// set here
+$subject = "Contact form your website by: ".$email;
+$to = 'ngeneanthony@gmail.com';
+
+//$body = <<<HTML
+//$message
+//HTML;
+
+
+$headers = "From: ".$email."\r\n";
+$headers .= "Content-type: text/html\r\n";
+if(mail($to, $subject, $message, $headers)){
+echo "success!";
+}else{echo "failure";}
+endif;
+?>
