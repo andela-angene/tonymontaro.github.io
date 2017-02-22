f5799b90813daf8130853bf1668d131b15a4e4df
Anthony Ngene <ngeneanthony@gmail.com>
2017-02-22 17:01:32 +0100

    --ammend

--- bc-19-shopping-cart/js/myscript.js
+++ bc-19-shopping-cart/js/myscript.js
@@ -1,115 +1,135 @@
-//Variables
-var draggable = $('.shop-item'),
-    cart = document.getElementById('cart'),
-    total = $('#total');
+$(document).ready(function () {
+  //Variables
+  var draggable = $('.shop-item'),
+      cart = document.getElementById('cart'),
+      total = $('#total');
 
-//Functions:
-//Drag start 
-function dragStart(event) {
-  event.dataTransfer.setData('text', this.id);
-}
-
-//Remove from cart
-function removeFromCart(event) {
-  if (event.dataTransfer.dropEffect == 'none') {
-    var num = $(this).find('.num');
+  //Functions:
+  //Drag start 
+  function dragStart(event) {
+    event.dataTransfer.setData('text', this.id);
+  }
+  
+  //Drag out of cart
+  function dragOut(event){
+    if (event.dataTransfer.dropEffect == 'none') {
+        
+      removeFromCart(this.id);
+    }
+  }
+  
+  //Remove from cart
+  function removeFromCart(id) {
+    id = '#' + id;
+    var num = $(id).find('.num');
 
     if (parseInt(num.html()) > 1) {
       num.html(num.html() - 1);
       //Update total price
-      var totalPrice = parseInt(total.html()) - parseInt($(this).find('.price').html());
+      var totalPrice = parseInt(total.html()) - parseInt($(id).find('.price').html());
       total.html(totalPrice);
       $('#alert-rm').fadeIn().delay(300).slideUp();
     } else {
       //Update total price
-      var totalPrice = parseInt(total.html()) - parseInt($(this).find('.price').html());
+      var totalPrice = parseInt(total.html()) - parseInt($(id).find('.price').html());
       total.html(totalPrice);
-      $(this).remove();
+      $(id).remove();
       $('#alert-rm').fadeIn().delay(300).slideUp();
     }
   }
-}
 
-//Allow drop
-function allowDrop(event) {
-  event.preventDefault();
-}
-
-//Drop function
-function dragDrop(event) {
-  event.preventDefault();
-  $('.cart').removeClass('drag-enter');
-
-  //Get the drag item id
-  var id = event.dataTransfer.getData('text');
-  if (id == '') return;
-
-  //New id for the item when it's added to the cart
-  var newID = id + '-cart',
-      jStoreItem = $('#' + newID);
-
-  //Check if item already exists in the cart
-  if (jStoreItem.html() == undefined) {
-
-    //Clone the store item
-    var storeItem = document.getElementById(id).cloneNode(true);
-
-    //Assign new id
-    storeItem.id = newID;
-
-    //Append the item to cart and set number of items in cart
-    this.appendChild(storeItem);
-    $('#' + newID + ' .num').html('1');
-
-    //Update total price
-    var totalPrice = parseInt(total.html()) + parseInt($('#' + newID + ' .price').html());
-    total.html(totalPrice);
-    $('#alert').fadeIn().delay(300).slideUp();
-    
-    //Add event listener to enable removal of item
-    storeItem.addEventListener('dragend', removeFromCart);
-  } else {
-    //Update total price
-    var totalPrice = parseInt(total.html()) + parseInt($('#' + newID + ' .price').html());
-    total.html(totalPrice);
-
-    //Item already exists, increment number in cart
-    var num =  $('#' + newID + ' .num');
-    num.html(parseInt(num.html()) + 1);
-    $('#alert').fadeIn().delay(300).slideUp();
+  //Allow drop
+  function allowDrop(event) {
+    event.preventDefault();
   }
-}
 
-//Drag Over function
-function dragEnter(event){
-  $('.cart').addClass('drag-enter');
-}
-//Drag Leave function
-function dragLeave(event){
-  $('.cart').removeClass('drag-enter');
-}
+  //Add to Cart
+  function addToCart(id){
+    //New id for the item when it's added to the cart
+    var newID = id + '-cart',
+        jStoreItem = $('#' + newID);
 
-//Add event listener to the draggable items
-for (var i = 0; i < draggable.length; i++) {
+    //Check if item already exists in the cart
+    if (jStoreItem.html() === undefined) {
 
-  //Store the draggable id when dragging starts
-  draggable[i].addEventListener('dragstart', dragStart);
-}
+      //Clone the store item
+      var storeItem = document.getElementById(id).cloneNode(true);
 
-//Add event listeners to the cart
-cart.addEventListener('dragover', allowDrop);
-cart.addEventListener('drop', dragDrop);
-cart.addEventListener('dragover', dragEnter);
-cart.addEventListener('dragleave', dragLeave);
+      //Assign new id
+      storeItem.id = newID;
 
+      //Append the item to cart and set number of items in cart
+      $('#cart').append(storeItem);
+      $('#' + newID + ' .num').html('1');
 
-$(document).ready(function () {
+      //Update total price
+      var totalPrice = parseInt(total.html()) + parseInt($('#' + newID + ' .price').html());
+      total.html(totalPrice);
+      $('#alert').fadeIn().delay(300).slideUp();
+
+      //Add event listener to enable removal of item
+      storeItem.addEventListener('dragend', dragOut);
+      $('#' + newID).on('click', '.rm-from-cart', function(){
+        console.log('ready');
+        removeFromCart($(this).parent().attr('id'));
+      });
+    } else {
+      //Update total price
+      var totalPrice = parseInt(total.html()) + parseInt($('#' + newID + ' .price').html());
+      total.html(totalPrice);
+
+      //Item already exists, increment number in cart
+      var num =  $('#' + newID + ' .num');
+      num.html(parseInt(num.html()) + 1);
+      $('#alert').fadeIn().delay(300).slideUp();
+    }
+  }
+
+  //On Drop function
+  function dragDrop(event) {
+    event.preventDefault();
+    $('.cart').removeClass('drag-enter');
+
+    //Get the drag item id
+    var id = event.dataTransfer.getData('text');
+    if (id == '') return;
+
+    addToCart(id);
+  }
+
+  //Drag Over function
+  function dragEnter(event){
+    $('.cart').addClass('drag-enter');
+  }
+  //Drag Leave function
+  function dragLeave(event){
+    $('.cart').removeClass('drag-enter');
+  }
+
+  //Add event listener to the draggable items
+  for (var i = 0; i < draggable.length; i++) {
+
+    //Store the draggable id when dragging starts
+    draggable[i].addEventListener('dragstart', dragStart);
+  }
+
+  //Add event listeners to the cart
+  cart.addEventListener('dragover', allowDrop);
+  cart.addEventListener('drop', dragDrop);
+  cart.addEventListener('dragover', dragEnter);
+  cart.addEventListener('dragleave', dragLeave);
   
   //Show by categories
   $('nav a').on('click', function(){
     $('.category').hide();
     $('#' + this.id.slice(5)).fadeIn();
   });
+  
+  //Button to Add or Remove from cart
+  $('.shop-item').on('click', '.add-to-cart', function(){
+    addToCart($(this).parent().attr('id'));
+  });
+  
 });
 
 
